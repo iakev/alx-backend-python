2425309fff98689cb3832bc2b912454de9708921
iakev <kirimikmwongera@gmil.com>
2023-04-27 12:18:06 +0000

    Added a type-annotated function that takes a string, integer or float and returns a tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module that contains a type-annotated function taking in a string
+and an int and returning a tuple
+"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Takes in k: a string, v: an int then constructs a
+    tuple with first element being k and seconf being square
+    of v
+    """
+    return {k, v**2}
