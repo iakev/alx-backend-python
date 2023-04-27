a149bf18382fd815ff6406dea7f5c66326979be8
iakev <kirimikmwongera@gmil.com>
2023-04-27 11:13:06 +0000

    added a type-annoteted string concatenating function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+This module contains a type-annotated function concat that takes two strings
+and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Takes in two string arguments and returns
+    the concatenated result of the arguments
+    """
+    return str1 + str2
