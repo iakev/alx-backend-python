28e25f4781a19adc0d893da21e8aa3f7aa5012b4
iakev <kirimikmwongera@gmil.com>
2023-04-27 11:34:06 +0000

    Added a type-annotated function that takes a float argumane and returns it's floor value

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module contains a type-annotated function that
+returns a floor value of an interger
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Function recieves a float argument and
+    returns the integer floor of the float
+    """
+    return (int(n))
