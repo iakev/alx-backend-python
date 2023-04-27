d03400383752d0e241334bfdfd5f692597d45261
iakev <kirimikmwongera@gmil.com>
2023-04-27 11:46:20 +0000

    Added a python module that defines type-annotated variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A module that defines type annotated variables of type
+integer, float, boolean and string
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
