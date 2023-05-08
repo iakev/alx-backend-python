cd2cda2caa24e83d26797c809ea0c3aacd03de81
iakev <kirimikmwongera@gmil.com>
2023-05-08 17:32:27 +0000

    Modified to yield result after asynchronously waiting for 1 second

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,10 +10,9 @@ async def async_generator():
     """
     async generator that yields a random number btn 0 and 10
     """
-    random.seed()
     i = 0
     while i < 10:
         result = random.uniform(0, 10)
-        yield result
         i += 1
         await asyncio.sleep(1)
+        yield result
