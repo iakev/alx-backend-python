3158ba08fd89148864eb3e28ab40781237d65f58
iakev <kirimikmwongera@gmail.com>
2023-05-22 06:01:45 -0700

    Added a codebase to test

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Contains Unit test for utils.access_nested_map function
+"""
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    A testing class for utils.access_nested_map
+    """
+
+    @parameterized.expand([
+        ("level_0_map", {"a": 1}, ("a",), 1),
+        ("level_1_map", {"a": {"b": 2}}, ("a",), {"b": 2}),
+        ("level_2_map", {"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, _, map, path, expected):
+        """
+        Method that tests that access_nested_map returns
+        what it should
+        """
+        self.assertEqual(access_nested_map(map, path), expected)
